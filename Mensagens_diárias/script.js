4ae303d6680f881ab16dce6de6c7c3c8f69c4c8d
Danielsilva777-git <danielsilva25061991@gmail.com>
2024-09-08 14:58:35 -0300

    adicionado mais textos

--- Mensagens_diárias/script.js
+++ Mensagens_diárias/script.js
@@ -28,6 +28,8 @@ const dias_da_semana  = {
     Quarta: "Deitar-me faz em verdes pastos, guia-me mansamente a águas tranqüilas ",
     Quinta: "Refrigera a minha alma; guia-me pelas veredas da justiça, por amor do seu nome   Ainda que eu andasse pelo vale da sombra da morte, não temeria mal algum, porque tu estás comigo; a tua vara e o teu cajado me consolam.",
     Sexta: "",
+    Sábado: "",
+    Domingo: " Para se receber a instrução do entendimento, a justiça, o juízo e a eqüidade, Para dar aos simples, prudência, e aos moços, conhecimento e bom siso; O sábio ouvirá e crescerá em conhecimento, e o entendido adquirirá sábios conselhos;  Para entender os provérbios e sua interpretação; as palavras dos sábios e as suas proposições. O temor do Senhor é o princípio do conhecimento; os loucos desprezam a sabedoria e a instrução.",
 }
 
 let day = dias[hoje]
