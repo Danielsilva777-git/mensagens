95b9a37bf3fa624ae9e9e548004d0883ee0cb4e5
Danielsilva777-git <danielsilva25061991@gmail.com>
2024-10-02 19:50:39 -0300

    Revert "ok"
    This reverts commit fa08d3c9ab1d6b9fd36d8eb70b5952c282454669.

--- script/msg.js
+++
@@ -1,8 +0,0 @@
-fetch('versiculos.json')
-.then(response => response.json())
-.then(data =>{
-    const day = "Terça"
-    const versiculo = data[day]
-    document.getElementById("text").textContent = versiculo
-})
-
